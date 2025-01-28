804b8c0758f96b11bc4d03ee234cb3d674233816
Julio González <juliogtrenard@gmail.com>
2025-01-28 10:39:03 +0100

    UPDATE module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,13 @@ module es.juliogtrenard.proyectobiblioteca {
     requires javafx.fxml;
     requires java.sql;
     requires org.slf4j;
+    requires net.sf.jasperreports.core;
 
 
     opens es.juliogtrenard.proyectobiblioteca to javafx.fxml;
     exports es.juliogtrenard.proyectobiblioteca;
     opens es.juliogtrenard.proyectobiblioteca.controller to javafx.fxml;
     exports es.juliogtrenard.proyectobiblioteca.controller;
+    opens es.juliogtrenard.proyectobiblioteca.model to javafx.fxml;
+    exports es.juliogtrenard.proyectobiblioteca.model;
 }
